73ef29db9e996385dfb0a8f4ed2d3359418e5d15
nosami <jasonimison@gmail.com>
2017-10-13 21:05:17 +0100

    Bump to 1.0.0.15

---
+++ AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿namespace Xamarin.Android.FSharp
+open System.Reflection
+
+[<assembly: AssemblyTitle("Xamarin.Android.FSharp.ResourceProvider")>]
+[<assembly: AssemblyDescription("Android type provider for resource IDs")>]
+[<assembly: AssemblyCompany("Xamarin")>]
+[<assembly: AssemblyCopyright("Xamarin")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.15")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
