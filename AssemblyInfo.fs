b5a079294aec0dcb4c5f1009602c086bcdd69578
nosami <jasonimison@gmail.com>
2018-02-16 13:07:27 +0000

    Bump to 1.0.0.19

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -8,7 +8,7 @@ open System.Reflection
 
 // The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
 
-[<assembly: AssemblyVersion("1.0.0.18")>]
+[<assembly: AssemblyVersion("1.0.0.19")>]
 
 //[<assembly: AssemblyDelaySign(false)>]
 //[<assembly: AssemblyKeyFile("")>]
